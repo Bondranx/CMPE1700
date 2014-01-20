4ae59af7ab0353ae4981137e1aadaa54e90129ab
Brandon Foote <brandon.foote@gmail.com>
2014-01-20 09:32:48 -0700

    ICA 1 completed
    ICA 1: planetary weight calculator completed

---
+++ CMPE1700 Brandon Foote Assignment 1b/CMPE1700 Brandon Foote Assignment 1b/Assignment1b.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CMPE1700_Brandon_Foote_Assignment_1b
+{
+    class Assignment1b
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+            int count = 0;
+            int temp = 0;
+            int x = 0;
+            bool hasSwapped = false;
+            Random generator = new Random();
+            Console.WriteLine("How many rolls should be generated?: ");
+            int.TryParse(Console.ReadLine(), out input);
+            int[] array = new int[input];
+            do
+            {
+                array[count] = generator.Next(1, 7) + generator.Next(1, 7);
+                count++;
+            }
+            while (count < input);
+
+            if (input < 2)
+            {
+            }
+            else
+            {
+                Console.WriteLine("Commenced sorting of {0} items", count);
+                do
+                {
+                    hasSwapped = false;
+                    x = 0;
+                    do
+                    {
+                        if (array[x] > array[x + 1])
+                        {
+                            temp = array[x];
+                            array[x] = array[x + 1];
+                            array[x + 1] = temp;
+                            hasSwapped = true;
+                        }
+                        x++;
+                    }
+                    while (x < input - 1);
+                }
+                while (hasSwapped == true);
+                Console.WriteLine("Sort concluded");
+                Console.WriteLine("\nHistogram of roll results\n");
+                x = 0;
+                temp = 0;
+                while (x < input)
+                {
+                    if (temp != array[x])
+                    {
+                        temp = array[x];
+                        Console.Write("\n");
+                        Console.Write("{0}:", temp);
+                    }
+                    Console.Write("*");
+                    x++;
+                }
+                Console.WriteLine("");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ CMPE1700BrandonFooteICA1/CMPE1700BrandonFooteICA1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CMPE1700BrandonFooteICA1
+{
+    class Program
+    {
+        public enum Planets {Mercury = 1, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune};
+        static void Main(string[] args)
+        {
+            double mass = 0;
+            double weight = 0;
+            double modifier = 0;
+            int input = 0;
+            bool success = false;
+            do
+            {
+                Console.Write("What is your current mass (in Kg)? ");
+                success = double.TryParse(Console.ReadLine(), out mass);
+            }
+            while (success == false || mass < 0);
+            success = false;
+            foreach (var value in Enum.GetValues(typeof(Planets)))
+                Console.WriteLine("({0}) {1}", (int)value, value);
+            do
+            {
+            Console.Write("Please enter the number of the planet you wish to visit: ");
+            success = int.TryParse(Console.ReadLine(), out input);
+            }
+            while(success == false || input < 1 || input > 8);
+            switch (input)
+            {
+                case 1:
+                    modifier = 3.7;
+                    break;
+                case 2:
+                    modifier = 8.87;
+                    break;
+                case 3:
+                    modifier = 9.81;
+                    break;
+                case 4:
+                    modifier = 3.71;
+                    break;
+                case 5:
+                    modifier = 24.92;
+                    break;
+                case 6:
+                    modifier = 10.44;
+                    break;
+                case 7:
+                    modifier = 8.87;
+                    break;
+                case 8:
+                    modifier = 11.15;
+                    break;
+            }
+            weight = mass * modifier;
+            Console.WriteLine("On the surface of {0} you would weigh: {1} Newtons", (Planets)input, weight);
+            Console.ReadLine();
+        }
+    }
+}
