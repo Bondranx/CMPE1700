a0947bc0fbef3eff6821217fa0762e1fcc01610f
Brandon Foote <brandon.foote@gmail.com>
2014-01-27 09:45:57 -0700

    ICA 2 Progress Update
    Structs.cs
    - Changed string variables to enums
    - Removed class declaration
    Cards.cs
    - Created for loop to create a set of cards.
    - Created for loop to create a set of cards for each suit.
    - Created List of cards called Deck.

--- CMPE1700BrandonFooteICA2/CMPE1700BrandonFooteICA2/Cards.cs
+++ CMPE1700BrandonFooteICA2/CMPE1700BrandonFooteICA2/Cards.cs
@@ -8,9 +8,20 @@ namespace CMPE1700BrandonFooteICA2
     class Cards
     {        
         static void Main(string[] args)
-        {
+        {            
             List<Card> Deck = new List<Card>();
-            
+
+            for (int count = 0; count < 4; count++)
+            {
+                for (int count2 = 2; count2 < 15; count2++)
+                {
+                    Card NewCard = new Card((CardSuit)count, (CardNumber)count2);
+                    Deck.Add(NewCard);
+                    Console.WriteLine(NewCard.ToString());
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
--- CMPE1700BrandonFooteICA2/CMPE1700BrandonFooteICA2/Structs.cs
+++ CMPE1700BrandonFooteICA2/CMPE1700BrandonFooteICA2/Structs.cs
@@ -5,25 +5,24 @@ using System.Text;
 
 namespace CMPE1700BrandonFooteICA2
 {
-    class Structs
-    {
+
         public enum CardSuit { Clubs, Diamonds, Hearts, Spades };
-        public enum CardNumber { Deuce = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack = 11, Queen = 12, King = 13, Ace = 14 };
+        public enum CardNumber { Deuce = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace};
         public struct Card
         {
-            public string _suit;
-            public string _number;
+            public CardSuit _suit;
+            public CardNumber _number;
             
 
-            public Card(string suit, string number)
+            public Card(CardSuit suit, CardNumber number)
             {
-                _suit = "";
-                _number = "";
+                _suit = suit;
+                _number = number;
             }
             public override string ToString()
             {
-                return string.Format("{0}, {1}", _suit, _number);
+                return string.Format("{0}, of {1}", _number, _suit);
             } 
         }
-    }
+    
 }
