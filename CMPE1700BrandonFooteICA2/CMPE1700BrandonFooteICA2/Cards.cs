04b03df0f60698eb411484a1a1a7cee9c94f46ee
Brandon Foote <brandon.foote@gmail.com>
2014-01-27 05:22:20 -0700

    ICA 2
    Started

---
+++ CMPE1700BrandonFooteICA2/CMPE1700BrandonFooteICA2/Cards.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CMPE1700BrandonFooteICA2
+{
+    class Cards
+    {        
+        static void Main(string[] args)
+        {
+            List<Card> Deck = new List<Card>();
+            
+        }
+    }
+}
---
+++ CMPE1700BrandonFooteICA2/CMPE1700BrandonFooteICA2/Structs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CMPE1700BrandonFooteICA2
+{
+    class Structs
+    {
+        public enum CardSuit { Clubs, Diamonds, Hearts, Spades };
+        public enum CardNumber { Deuce = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack = 11, Queen = 12, King = 13, Ace = 14 };
+        public struct Card
+        {
+            public string _suit;
+            public string _number;
+            
+
+            public Card(string suit, string number)
+            {
+                _suit = "";
+                _number = "";
+            }
+            public override string ToString()
+            {
+                return string.Format("{0}, {1}", _suit, _number);
+            } 
+        }
+    }
+}
