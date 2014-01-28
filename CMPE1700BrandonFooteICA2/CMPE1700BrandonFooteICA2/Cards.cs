483dacecb1f367136ffe5b352c401fdb935dae1a
Brandon Foote <brandon.foote@gmail.com>
2014-01-28 12:56:11 -0700

    AS below
    Added ability to pick number of shuffles.
    Will not accept a number of shuffles greater than 20.

--- CMPE1700BrandonFooteICA2/CMPE1700BrandonFooteICA2/Cards.cs
+++ CMPE1700BrandonFooteICA2/CMPE1700BrandonFooteICA2/Cards.cs
@@ -7,13 +7,38 @@ namespace CMPE1700BrandonFooteICA2
 {
     class Cards
     {
+        static void shuffle(List<Card> Deck)
+        {
+            Card swap = new Card();
+
+                Random number = new Random();
+            for (int count = 51; count >= 0; count--)
+            {
+                int random = number.Next(0, count);
+                swap = (Deck[count]);
+                Deck[count] = Deck[random];
+                Deck[random] = swap;
+            }
+        }
 
         static void Main(string[] args)
         {            
-            Random number = new Random();
+
             List<Card> Deck = new List<Card>();
-            Card swap = new Card();
-            
+            int input = 0;
+            bool check = false;
+
+            do
+            {
+                Console.WriteLine("How many times would you like to shuffle the deck?");
+                check = int.TryParse(Console.ReadLine(), out input);
+                if (input > 20)
+                {
+                    Console.WriteLine("That nubmer is too high, try again\n");
+                }
+            }
+            while (check == false || input > 20);
+
             for (int count = 0; count < 4; count++)
             {
                 for (int count2 = 2; count2 < 15; count2++)
@@ -23,13 +48,9 @@ namespace CMPE1700BrandonFooteICA2
                     
                 }
             }
-
-            for (int count = 51; count >= 0; count--)
+            for (int count = 0; count < input; count++)
             {
-                int random = number.Next(0, count);
-                swap = (Deck[count]);
-                Deck[count] = Deck[random];
-                Deck[random] = swap;
+                shuffle(Deck);
             }
 
             foreach (Card value in Deck)
@@ -37,6 +58,7 @@ namespace CMPE1700BrandonFooteICA2
                 Console.WriteLine(value);
             }
             Console.ReadLine();
+            
         }
     }
 }
