845c0ff2482ef76a0beec3e4bc9c3518ad729727
Brandon Foote <brandon.foote@gmail.com>
2014-01-28 13:54:58 -0700

    ICA 3 Started
    - Student information struct completed
    - ToString override for students struct completed
    - Constructor written to assign variables to struct on creation
    - Added code to open a file to read from

---
+++ CMPE1700BrandonFooteICA3/CMPE1700BrandonFooteICA3/Students.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CMPE1700BrandonFooteICA3
+{
+    class Students
+    {
+        public static double GPA(List<double> Grades)
+        {
+            double GPA = 0;
+            double GradesTotal = 0;
+            for (int count = 0; count < Grades.Count; count++)
+            {
+                GradesTotal += Grades[count];
+            }
+            GPA = GradesTotal / Grades.Count;
+
+            return GPA;
+        }
+
+
+        public struct Student
+        {
+            public string _studentName;
+            public string _IDNumber;
+            public List<double> _Grades;
+
+            public Student(string StudentName, string IDNumber, List<double> Grades)
+            {
+                _studentName = StudentName;
+                _IDNumber = IDNumber;
+                _Grades = Grades;
+            }
+
+            public override string ToString()
+            {
+                return string.Format("{0} ({1}) GPA:{2:F1}", _studentName, _IDNumber, GPA(_Grades));
+            }
+        }
+        static void Main(string[] args)
+        {
+            StreamReader inputFile;
+            
+            string input = "";
+            Console.WriteLine("Enter the name of the file you would like to read from: ");
+            input = Console.ReadLine();
+            try
+            {
+                inputFile = new StreamReader(input);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
