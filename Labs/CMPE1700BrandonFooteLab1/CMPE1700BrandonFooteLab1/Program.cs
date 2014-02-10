724700abad4c284565bc5ae6bd14d1d405e8d512
Brandon Foote <brandon.foote@gmail.com>
2014-02-10 10:04:24 -0700

    Lab 1
    Added line to request an encryption key from the user
    Worked on Encryption code

--- Labs/CMPE1700BrandonFooteLab1/CMPE1700BrandonFooteLab1/Program.cs
+++ Labs/CMPE1700BrandonFooteLab1/CMPE1700BrandonFooteLab1/Program.cs
@@ -13,39 +13,46 @@ namespace CMPE1700BrandonFooteLab1
             int holder;
             try
             {
-                try
-                {
-                    while ((holder = input.Read()) != -1)
-                        newList.Add((char)holder);
-                }
-                catch (Exception a)
-                {
-                    Console.WriteLine(a);
-                }
-                finally
-                {
-                    input.Close();
-                }
+                while ((holder = input.Read()) != -1)
+                    newList.Add((char)holder);
             }
-            catch (Exception e)
+            catch (Exception a)
             {
-                Console.WriteLine(e);
+                Console.WriteLine(a);
             }
+            finally
+            {
+                input.Close();
+            }
+
             return newList;
         }
 
         static void Main(string[] args)
         {
             List<char> characterList = new List<char>();
-            
+
+            int count = 0;
+            char holder;
             string UserInput = "";
-            Console.WriteLine("What is the name of the file you would like to encrypt: ");
+            string encryptionKey = "";
+
+            Console.WriteLine("What is the name of the file you would like to Encrypt/Decrypt: ");
             UserInput = Console.ReadLine();
+
+            Console.WriteLine("Please enter the encryption key you will be using: ");
+            encryptionKey = Console.ReadLine();
             StreamReader NewStreamReader = new StreamReader(UserInput);
+
             characterList = Reader(characterList, NewStreamReader, UserInput);
+
+
             foreach (char value in characterList)
             {
-                Console.Write(value);
+                characterList[value] = (char)(characterList[value] ^ encryptionKey[count]);
+                Console.WriteLine(value);
+                if (count >= encryptionKey.Length-1)
+                    count = 0;
             }
             Console.ReadKey();
         }
