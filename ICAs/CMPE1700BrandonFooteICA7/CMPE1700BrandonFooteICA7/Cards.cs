4edbf0caac978f4ed07d6dc1bccd9c27206daf1a
Brandon Foote <brandon.foote@gmail.com>
2014-03-31 06:46:59 -0600

    ICA7
    asdf

---
+++ ICAs/CMPE1700BrandonFooteICA7/CMPE1700BrandonFooteICA7/Cards.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CMPE1700BrandonFooteICA7
+{
+    class Cards
+    {
+        public static Stack<Card> Shuffle(Stack<Card> Deck)
+        {
+            Stack<Card> partTwo = new Stack<Card>();
+            Stack<Card> FinalDeck = new Stack<Card>();
+            Random newRandom = new Random();
+            int Position = 0;
+            int count2 = 0;
+
+            Position = newRandom.Next(20, 35);
+
+            for (int count = 0; count < Position; count++)
+            {
+                partTwo.Push(Deck.Pop());
+            }
+
+            if (partTwo.Count > Deck.Count)
+            {
+                while (Deck.Count != 0 && partTwo.Count != 0)
+                {
+
+                    if (count2 == 0 && partTwo.Count != 0)
+                        FinalDeck.Push(partTwo.Pop());
+                    else if (count2 == 1 && Deck.Count != 0)
+                        FinalDeck.Push(Deck.Pop());
+
+                    if (count2 == 1)
+                        count2 = 0;
+                    else if(count2 == 0)
+                        count2++;
+                }
+            }
+            else if (partTwo.Count < Deck.Count)
+            {
+                while (Deck.Count != 0 && partTwo.Count != 0)
+                {
+
+                    if (count2 == 0 && Deck.Count != 0)
+                        FinalDeck.Push(Deck.Pop());
+                    else if (count2 == 1 && partTwo.Count != 0)
+                        FinalDeck.Push(partTwo.Pop());
+
+                    if (count2 == 1)
+                        count2 = 0;
+                    else if (count2 == 0)
+                        count2++;
+
+                }
+            }
+
+
+
+            return FinalDeck;
+        }
+
+        static void Main(string[] args)
+        {
+            Stack<Card> Deck = new Stack<Card>();
+            int count3 = 0;
+
+            for (int count = 0; count < 4; count++)
+            {
+                for (int count2 = 2; count2 < 15; count2++)
+                {
+                    Card NewCard = new Card((CardSuit)count, (CardNumber)count2);
+                    Deck.Push(NewCard);
+                }
+            }
+            //for(int count = 0; count<4;count++)
+                Deck = Shuffle(Deck);
+            foreach (Card n in Deck)
+            {
+                Console.WriteLine(n);
+                Console.WriteLine(count3++);
+
+            }
+                Console.ReadLine();
+                
+        }
+        
+    }
+}
---
+++ ICAs/CMPE1700BrandonFooteICA7/CMPE1700BrandonFooteICA7/Structs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CMPE1700BrandonFooteICA7
+{
+        public enum CardSuit { Clubs, Diamonds, Hearts, Spades };
+        public enum CardNumber { Deuce = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace };
+        public struct Card
+        {
+            public CardSuit _suit;
+            public CardNumber _number;
+
+
+            public Card(CardSuit suit, CardNumber number)
+            {
+                _suit = suit;
+                _number = number;
+            }
+            public override string ToString()
+            {
+                return string.Format("{0}, of {1}", _number, _suit);
+            }
+        }
+    
+}
